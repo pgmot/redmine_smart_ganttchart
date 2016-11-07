3e8bc6fccf7e77e8c87dea9a3f413e5c2a9676aa
mot <mot.contact.address.pc@gmail.com>
2016-11-07 15:41:25 +0900

    Revert "1. Support separately changing of start and due date."
    This reverts commit 29f0386a7def9917eb7a19feed02f5e297a503ae.

--- app/controllers/ganttchart_controller.rb
+++ app/controllers/ganttchart_controller.rb
@@ -1,11 +1,10 @@
 class GanttchartController < ApplicationController
   def move
-    moved_s = params[:moved_s].to_i
-    moved_e = params[:moved_e].to_i
+    moved_days = params[:moved_days].to_i
     p User.current
     issue = Issue.find(params[:id])
-    issue.start_date = issue.start_date.advance(days: moved_s)
-    issue.due_date = issue.due_date.advance(days: moved_e)
+    issue.start_date = issue.start_date.advance(days: moved_days)
+    issue.due_date = issue.due_date.advance(days: moved_days)
     issue.save!
     render nothing: true
   end
